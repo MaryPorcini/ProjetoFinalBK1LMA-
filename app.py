05943f6ca3f98919412b08f28a80cda9eebe485c
MarianyPorcini <marianyporcini@gmail.com>
2024-11-07 14:04:19 -0300

    Index pronto e imagens

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Lista de produtos com nome, preço e quantidade
+    produtos = [
+        {"nome": "Cookie Recheado de Chocolate", "preco": 8, "quantidade": 15, "imagem": "doces.jpg"},
+        {"nome": "Pão de Mel", "preco": 8, "quantidade": 10, "imagem": "doces.jpg"},
+        {"nome": "Palha Italiana de Ninho", "preco": 7, "quantidade": 10, "imagem": "doces.jpg"},
+        {"nome": "Palha Italiana de Chocolate", "preco": 7, "quantidade": 10, "imagem": "doces.jpg"},
+    ]
+    return render_template('index.html', produtos=produtos)
 
 if __name__ == '__main__':
     app.run(debug=True)
