8a76b3b2b78661aa7a835ed83764aaeee22e37b1
MarianyPorcini <marianyporcini@gmail.com>
2024-11-21 12:38:50 -0300

    Versão 2.0 semi-finalizada

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 lista_produtos = [
-        {"id": 1, "nome": "Cookie Recheado de Chocolate", "preco": 8, "quantidade": 15, "imagem": "cookies.jpg"},
+        {"id": 1, "nome": "Cookie Recheado de Chocolate", "preco": 10, "quantidade": 15, "imagem": "cookies.jpg"},
         {"id": 2, "nome": "Pão de Mel", "preco": 8, "quantidade": 10, "imagem": "pao de mel.jfif"},
-        {"id": 3, "nome": "Palha Italiana de Ninho", "preco": 7, "quantidade": 10, "imagem": "palha ninho.jpg"},
-        {"id": 4, "nome": "Palha Italiana de Chocolate", "preco": 7, "quantidade": 10, "imagem": "palha brigadeiro.jpg"},
-        {"id": 5, "nome": "Biscoitos Decorados", "preco": 6, "quantidade": 10, "imagem": "biscoitos.jpg"},
-        {"id": 6, "nome": "Kit Biscoitos Recheados", "preco": 15, "quantidade": 10, "imagem": "kit biscoitos.jpg"},
+        {"id": 3, "nome": "Palha Italiana de Ninho", "preco": 15, "quantidade": 10, "imagem": "palha ninho.jpg"},
+        {"id": 4, "nome": "Palha Italiana de Chocolate", "preco": 15, "quantidade": 10, "imagem": "palha brigadeiro.jpg"},
+        {"id": 5, "nome": "Biscoitos Decorados (4 unidades)", "preco": 20, "quantidade": 10, "imagem": "biscoitos.jpg"},
+        {"id": 6, "nome": "Kit Confeiteiro (4 unidades)", "preco": 50, "quantidade": 10, "imagem": "kit biscoitos.jpg"},
+        {"id": 7, "nome": "Brownie com Nozes", "preco": 10, "quantidade": 10, "imagem": "brownies com nozes.jpg"},
+        {"id": 8, "nome": "Caixa de Alfajor (6 unidades)", "preco": 40, "quantidade": 11, "imagem": "Alfajor.jpg"},
+        {"id": 9, "nome": "Pote de Casadinhos de Doce de Leite (12 unidades)", "preco": 50, "quantidade": 8, "imagem": "12 casadinhos.jpg"},
+        {"id": 10, "nome": "Kit Casinha", "preco": 90, "quantidade": 10, "imagem": "kit casinha.jpg"},
     ]
 
 @app.route('/')
